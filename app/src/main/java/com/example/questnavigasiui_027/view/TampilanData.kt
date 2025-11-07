40bcd3f1f9214288e60c34712919a5877a40abb5
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-07 16:34:51 +0700

    membuat column ketiga digunakan untuk judul kotak dan isi data per kategori

--- app/src/main/java/com/example/questnavigasiui_027/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasiui_027/view/TampilanData.kt
@@ -47,3 +47,7 @@ fun TampilData(
             Column (modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
                 verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))){
                 items.forEach { item->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
