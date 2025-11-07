e39a9971da3263b900f677a921fe98255f0052ac
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-07 15:42:30 +0700

    buat garis untuk dibawah jenis kelamin menggunakan HorizontalDivider

--- app/src/main/java/com/example/questnavigasiui_027/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_027/view/Formulir.kt
@@ -71,3 +71,9 @@ fun FormIsian(
                     }
                 }
             }
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
