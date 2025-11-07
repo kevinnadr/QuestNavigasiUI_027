356d766d1d810fbe9b0e503ae500ab696d82b84d
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-07 16:25:17 +0700

    membuat scaffold untuk tampilan hp dan membuat judul dan mengatur warna di atas layar

--- app/src/main/java/com/example/questnavigasiui_027/view/TampilanData.kt
+++ app/src/main/java/com/example/questnavigasiui_027/view/TampilanData.kt
@@ -34,4 +34,11 @@ fun TampilData(
         Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(id = R.string.alamat), "Yogyakarta"),
-    }
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
+            )
+        }
