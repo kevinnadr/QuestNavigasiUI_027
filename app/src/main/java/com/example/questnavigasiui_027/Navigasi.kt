b0b246e13650ce447e412d198ea77c92898b3699
Kevin Adrian <kevin19305.ib@gmail.com>
2025-11-07 19:09:26 +0700

    lalu buat private fun cancelAndBackToFormulir untuk setelah pada halaman tampilandata bisa balik lagi ke form isian

--- app/src/main/java/com/example/questnavigasiui_027/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_027/Navigasi.kt
@@ -44,3 +44,9 @@ fun DataApp(
         }
     }
 }
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(Navigasi.Formulir.name,
+        inclusive = false)
+}
